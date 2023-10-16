afc92bd69498d56958d7ac87182ba2d244aaed21
Prabhat Rajput <prajput@paloaltonetworks.com>
2023-10-16 16:32:58 +0000

    REPO_HOME=/tmp/bc_sys_test_Oct16-16_32_17

---
+++ aaa_demo/tf12_acl_public-read-write.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-log-bucket"
+  acl = "log-delivery-write"
+  tags = {
+    Demo = "log-delivery-write"
+  }
+}
---
+++ s3_tf/s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "aws-s3-create-bucket-bc" {
+  acl   = "public-read-write"
+  bucket = "user_bucket_name"
+  versioning {
+    enabled = false
+  }
+  tags = merge({
+    Demo = "public-read-write"
+    }, {
+    yor_trace            = "user_yor_trace"
+    git_commit           = "dd9fb87aa4652d07b39fdb21cde1ace618cbb76e"
+    git_file             = "s3.tf"
+    git_last_modified_at = "2023-05-13 00:51:51"
+    git_last_modified_by = "belle.bao@gmail.com"
+    git_modifiers        = "belle.bao"
+    git_org              = "bellebao"
+    git_repo             = "test"
+  })
+}
